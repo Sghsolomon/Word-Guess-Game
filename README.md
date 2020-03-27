cef9422344a7242a3f3765284d4ae3875a78ab31
solomon <solomonghebrelibanos@gmail.com>
2020-03-27 01:02:19 -0700

    some updates up to song load

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Word-Guess-Game
 Guess Game
+This game is Word-Guess-Game.
+Press any key to get started.
+If the word you guessed is correct the song you choose will pop up.
+If not it will give you anothe chance to try.
--- assets/index.js
+++ assets/index.js
@@ -1,1 +1,44 @@
-document.write('solomon')
+
+// var array = ['f', 'h', 'r']
+//     console.log(array.join(''))
+
+var array = ['deluro', 'efriem', 'fihira']
+var wordGuess =  [];
+var userclick =  0;   
+var win =        0;
+
+var computerGuess = array[Math.floor(Math.random() * array.length)]          
+    console.log(computerGuess);
+    
+    
+    
+
+document.onkeyup = function(){
+   var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
+   userclick++
+   wordGuess.push(userGuess);
+   if(userclick < 7)
+   document.getElementById('guess').innerHTML  += userGuess + ' _';
+   
+   for(let key of computerGuess){
+       if(key == userGuess && userclick < 7){
+          document.getElementById('cchoice').innerHTML += userGuess;
+          break;
+       } 
+    }
+    if(wordGuess.join('') == computerGuess)
+       win++;
+       document.getElementById('win').innerHTML = win;
+       
+    
+}
+
+    
+     
+
+  
+
+
+
+
+
